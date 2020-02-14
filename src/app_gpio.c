dc6e867a430d6180625d896964bb1d3058ddab8b
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-02-14 16:54:11 +0700

    External clock, GPIO interrupt (without Linux)

--- src/app_gpio.c
+++ src/app_gpio.c
@@ -28,46 +28,123 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <stdlib.h>
+#include <stdint.h>
+
 #include "board.h"
+#include "debug_console_imx.h"
 #include "pin_mux.h"
 #include "gpio_pins.h"
 #include "gpio_imx.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
 #include "app.h"
+#include "app_log.h"
+#include "app_gpio.h"
 
 
-static gpio_config_t app_gpio_pin = {
-    "AppGpio",                           /* name */
+
+static uint8_t app_gpio_mode = 0;
+
+static gpio_config_t app_gpio_config = {
+    "App GPIO",                          /* name */
     &IOMUXC_SW_MUX_CTL_PAD_EPDC_DATA04,  /* muxReg */
     5,                                   /* muxConfig */
     &IOMUXC_SW_PAD_CTL_PAD_EPDC_DATA04,  /* padReg */
-    0,                                   /* padConfig */
+    /*
+    IOMUXC_SW_PAD_CTL_PAD_I2C2_SDA_PS(2) |
+    IOMUXC_SW_PAD_CTL_PAD_I2C2_SDA_PE_MASK |
+    IOMUXC_SW_PAD_CTL_PAD_I2C2_SDA_HYS_MASK,
+    */
+    IOMUXC_SW_PAD_CTL_PAD_EPDC_DATA04_PS(0) | IOMUXC_SW_PAD_CTL_PAD_EPDC_DATA04_PE_MASK, /* padConfig */
     GPIO2,                               /* base */
     4                                    /* pin */
 };
+
+/*
+#define APP_GPIO_IRQ_NUM BOARD_GPIO_KEY_IRQ_NUM
+#define APP_GPIO_HANDLER BOARD_GPIO_KEY_HANDLER
+#define APP_GPIO_CONFIG BOARD_GPIO_KEY_CONFIG
+*/
+
+#define APP_GPIO_IRQ_NUM GPIO2_INT15_0_IRQn
+#define APP_GPIO_HANDLER GPIO2_INT15_0_Handler
+#define APP_GPIO_CONFIG (&app_gpio_config)
+
+static volatile SemaphoreHandle_t gpio_sem = NULL;
+
+void APP_GPIO_HANDLER();
 
 void APP_GPIO_HardwareInit() {
     /* In this demo, we need to share board GPIO, we can set sreq argument to true
      * when the peer core could also access GPIO with RDC_SEMAPHORE, or the peer
      * core doesn't access the GPIO at all */
-    //RDC_SetPdapAccess(RDC, BOARD_GPIO_KEY_RDC_PDAP, 0xFF, false/*true*/, false);
+    //RDC_SetPdapAccess(RDC, rdcPdapGpio2, 0xFF, false/*true*/, false);
+
+    /* Enable gpio clock gate, led and key share same CCGR on this board */
+    //CCM_ControlGate(CCM, ccmCcgrGateGpio2, ccmClockNeededRunWait);
 
     /* Configure gpio pin IOMUX */
-    configure_gpio_pin(&app_gpio_pin);
+    configure_gpio_pin(APP_GPIO_CONFIG);
 }
 
-uint8_t APP_GPIO_Init() {
-    gpio_init_config_t pinInit = {
-        .pin           = app_gpio_pin.pin,
-        .direction     = gpioDigitalOutput,
-        .interruptMode = gpioNoIntmode
-    };
+uint8_t APP_GPIO_Init(uint32_t mode, SemaphoreHandle_t sem) {
+    app_gpio_mode = mode;
+    
+    gpio_init_config_t pinInit = {};
+    pinInit.pin = APP_GPIO_CONFIG->pin;
 
-    GPIO_Init(app_gpio_pin.base, &pinInit);
+    if (app_gpio_mode == APP_GPIO_MODE_INPUT) {
+        pinInit.direction = gpioDigitalInput;
+        pinInit.interruptMode = gpioIntFallingEdge;
+    } else if (app_gpio_mode == APP_GPIO_MODE_OUTPUT) {
+        pinInit.direction = gpioDigitalOutput;
+        pinInit.interruptMode = gpioNoIntmode;
+    } else {
+        PANIC_("Unknown GPIO mode: %d", app_gpio_mode);
+    }
+
+    GPIO_Init(APP_GPIO_CONFIG->base, &pinInit);
+
+    if (mode == APP_GPIO_MODE_INPUT) {
+        gpio_sem = sem;
+
+        /* Clear the interrupt state */
+        GPIO_ClearStatusFlag(APP_GPIO_CONFIG->base, APP_GPIO_CONFIG->pin);
+        /* Enable GPIO pin interrupt */
+        GPIO_SetPinIntMode(APP_GPIO_CONFIG->base, APP_GPIO_CONFIG->pin, true);
+
+        /* Set GPIO interrupt priority */
+        NVIC_SetPriority(APP_GPIO_IRQ_NUM, APP_GPIO_IRQ_PRIORITY);
+        /* Enable the IRQ. */
+        NVIC_EnableIRQ(APP_GPIO_IRQ_NUM);
+    }
+
     return 0;
 }
 
 uint8_t APP_GPIO_Set(uint8_t on) {
-    GPIO_WritePinOutput(app_gpio_pin.base, app_gpio_pin.pin, on ? gpioPinSet : gpioPinClear);
+    if (app_gpio_mode != APP_GPIO_MODE_OUTPUT) {
+        return 1;
+    }
+    GPIO_WritePinOutput(APP_GPIO_CONFIG->base, APP_GPIO_CONFIG->pin, on ? gpioPinSet : gpioPinClear);
     return 0;
+}
+
+void APP_GPIO_HANDLER() {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    /* Clear the interrupt state */
+    GPIO_ClearStatusFlag(APP_GPIO_CONFIG->base, APP_GPIO_CONFIG->pin);
+
+    //APP_INFO("APP_GPIO_HANDLER()");
+
+    /* Unlock the task to process the event. */
+    xSemaphoreGiveFromISR(gpio_sem, &xHigherPriorityTaskWoken);
+
+    /* Perform a context switch to wake the higher priority task. */
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
--- src/main.c
+++ src/main.c
@@ -19,26 +19,29 @@
 
 #define APP_TASK_STACK_SIZE    256
 
+#define BUFSIZE 32
 
 static void APP_Task_EcspiSend(void *param) {
-    uint8_t txbuf[4];
+    uint8_t txbuf[BUFSIZE] = {0};
     SemaphoreHandle_t send_sem = (SemaphoreHandle_t)param;
 
     APP_INFO("SPI send task start");
 
     uint32_t counter = 0;
     while (true) {
-        uint32_t data = counter;
-        for (int i = 3; i >= 0; --i) {
-            txbuf[i] = data & 0xFF;
-            data = data >> 8;
+        for (int j = 0; j < BUFSIZE/4; ++j) {
+            uint32_t data = counter;
+            for (int i = 3; i >= 0; --i) {
+                txbuf[4*j + i] = data & 0xFF;
+                data = data >> 8;
+            }
         }
         ASSERT(xSemaphoreTake(send_sem, portMAX_DELAY) == pdTRUE);
 
-        APP_GPIO_Set(counter & 1);
+        //APP_GPIO_Set(counter & 1);
 
         //APP_INFO("Transfer SPI message %d", counter);
-        if (APP_ECSPI_Transfer(txbuf, NULL, 4, APP_FOREVER_MS) != 0) {
+        if (APP_ECSPI_Transfer(txbuf, NULL, BUFSIZE, APP_FOREVER_MS) != 0) {
             PANIC_("Cannot transfer SPI message");
         }
         counter += 1;
@@ -55,17 +58,22 @@ int main(void) {
     SemaphoreHandle_t send_sem = xSemaphoreCreateBinary();
     ASSERT(send_sem);
 
-    ASSERT(APP_GPIO_Init() == 0);
     ASSERT(APP_ECSPI_Init(50000000) == 0);
-    
+
+    //APP_INFO("Init GPIO");
+    //ASSERT(APP_GPIO_Init(APP_GPIO_MODE_OUTPUT, NULL) == 0);
+
     /* Create tasks. */
     xTaskCreate(
         APP_Task_EcspiSend, "ECSPI send task",
-        APP_TASK_STACK_SIZE, (void *)send_sem, tskIDLE_PRIORITY + 1, NULL
+        APP_TASK_STACK_SIZE, (void *)send_sem, tskIDLE_PRIORITY + 4, NULL
     );
 
-    APP_INFO("Start GPT");
-    APP_GPT_Init(APP_GPT_SEC/100000, send_sem);
+    //APP_INFO("Start GPT");
+    //APP_GPT_Init(APP_GPT_SEC/100000, send_sem);
+
+    APP_INFO("Init GPIO");
+    ASSERT(APP_GPIO_Init(APP_GPIO_MODE_INPUT, send_sem) == 0);
 
 
     /* Start FreeRTOS scheduler. */
